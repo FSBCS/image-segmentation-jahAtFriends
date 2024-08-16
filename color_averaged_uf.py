7cab8deaf7632e142d816f92ac78210aaa72cf38
Joel Hammer <jhammer@friendsbalt.org>
2024-08-16 17:43:29 +0000

    Override union to include color averaging

--- color_averaged_uf.py
+++ color_averaged_uf.py
@@ -18,7 +18,23 @@ class ColorAveragedUF(WeightedQuickUnion):
         '''
         #TODO: Override the union method to (weighted) average the colors
         #  of the two components
-        pass
+        root_p = self.root(p)
+        root_q = self.root(q)
+        p_color = self.colors[p]
+        q_color = self.colors[q]
+        sz_p = self.size(root_p)
+        sz_q = self.size(root_q)
+        
+        color_mix = ac.proportional_average_rgb(p_color, sz_p, q_color, sz_q)
+        
+        super().union(root_p, root_q)
+        
+        if sz_p < sz_q:
+            self.colors[q] = color_mix
+        else:
+            self.colors[p] = color_mix
+                       
+
 
     def get_average_color(self, p):
         '''
